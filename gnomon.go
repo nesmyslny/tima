705a52f1db874fcbb03d6635c2f0aefdafed8817
dani <d.anielschmid@gmx.de>
2014-10-02 13:48:23 +0200

    ...literally "one that knows or examines"...

---
+++ gnomon.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>gnomon</h1>...literally \"one that knows or examines\"...")
+}
